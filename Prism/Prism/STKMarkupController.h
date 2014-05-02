69529f9d15b2547eaa714cbac9b08dbd81131d67
Joe Conway <joe.conway@stablekernel.com>
2014-05-02 15:37:07 -0400

    updates for user tagging

---
+++ Prism/Prism/STKMarkupController.h
@@ -0,0 +1,36 @@
+//
+//  STKMarkupController.h
+//  Prism
+//
+//  Created by Joe Conway on 5/2/14.
+//  Copyright (c) 2014 Higher Altitude. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class STKMarkupController, STKUser;
+
+@protocol STKMarkupControllerDelegate <NSObject>
+
+- (void)markupController:(STKMarkupController *)markupController
+           didSelectUser:(STKUser *)user
+        forMarkerAtRange:(NSRange)range;
+
+- (void)markupController:(STKMarkupController *)markupController
+        didSelectHashTag:(NSString *)hashTag
+        forMarkerAtRange:(NSRange)range;
+
+- (void)markupControllerDidFinish:(STKMarkupController *)markupController;
+
+@end
+
+@interface STKMarkupController : NSObject
+
+- (id)initWithDelegate:(id <STKMarkupControllerDelegate>)delegate;
+
+@property (nonatomic, weak) id <STKMarkupControllerDelegate> delegate;
+@property (nonatomic, strong, readonly) UIView *view;
+
+- (void)textView:(UITextView *)textView updatedWithText:(NSString *)text;
+
+@end
---
+++ Prism/Prism/STKMarkupController.m
@@ -0,0 +1,288 @@
+//
+//  STKMarkupController.m
+//  Prism
+//
+//  Created by Joe Conway on 5/2/14.
+//  Copyright (c) 2014 Higher Altitude. All rights reserved.
+//
+
+#import "STKMarkupController.h"
+#import "STKHashtagToolbar.h"
+#import "STKSearchProfileCell.h"
+#import "STKUserStore.h"
+#import "STKUser.h"
+#import "STKContentStore.h"
+#import "STKSearchHashTagsCell.h"
+
+typedef enum {
+    STKMarkupTypeHashtag,
+    STKMarkupTypeUser
+} STKMarkupType;
+
+@interface STKMarkupController ()
+    <UITableViewDataSource, UITableViewDelegate, STKHashtagToolbarDelegate>
+
+@property (nonatomic, strong) UILabel *promptLabel;
+@property (nonatomic, strong) UIButton *doneButton;
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic) NSRange markupRange;
+@property (nonatomic) STKMarkupType markupType;
+
+@property (nonatomic, strong) NSString *tempPostTextWithUserTags;
+
+@property (nonatomic, strong) NSArray *userTags;
+@property (nonatomic, strong) NSArray *hashTags;
+
+@end
+
+@implementation STKMarkupController
+
+- (id)initWithDelegate:(id<STKMarkupControllerDelegate>)delegate
+{
+    self = [super init];
+    if(self) {
+        [self setDelegate:delegate];
+        
+        _userTags = [[NSMutableArray alloc] init];
+        
+        [self loadView];
+    }
+    return self;
+}
+
+- (void)loadView
+{
+    _view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    [[self view] setBackgroundColor:[STKUnselectedColor colorWithAlphaComponent:1]];
+    
+    _doneButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [[self doneButton] setTitle:@"Done" forState:UIControlStateNormal];
+    [[self doneButton] addTarget:self action:@selector(done:) forControlEvents:UIControlEventTouchUpInside];
+    [[self doneButton] setFrame:CGRectMake(270, 0, 50, 44)];
+    [[self doneButton] setTintColor:[UIColor whiteColor]];
+    [[self view] addSubview:[self doneButton]];
+    
+    _promptLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 260, 44)];
+    [[self promptLabel] setFont:[UIFont systemFontOfSize:12]];
+    [[self promptLabel] setNumberOfLines:2];
+    [[self promptLabel] setTextColor:STKTextColor];
+    [[self promptLabel] setText:@"Use # to add a tag and @ to add a person"];
+    [[self view] addSubview:[self promptLabel]];
+    
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 0, 0) style:UITableViewStylePlain];
+    [[self tableView] setDelegate:self];
+    [[self tableView] setDataSource:self];
+    [[self tableView] setScrollEnabled:YES];
+    [[self tableView] setHidden:YES];
+    [[self tableView] setBackgroundColor:[UIColor clearColor]];
+    [[self tableView] setRowHeight:44];
+    [[self tableView] setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    [[self view] addSubview:[self tableView]];
+
+    for(UIView *subview in [[self view] subviews]) {
+        [subview setTranslatesAutoresizingMaskIntoConstraints:NO];
+    }
+    
+    [[self view] addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[v(==44)]|" options:0 metrics:nil views:@{@"v" : [self doneButton]}]];
+    [[self view] addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[v(==44)]|" options:0 metrics:nil views:@{@"v" : [self promptLabel]}]];
+    [[self view] addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[lbl(==260)][db]|" options:0 metrics:nil views:@{@"lbl" : [self promptLabel], @"db" : [self doneButton]}]];
+    [[self view] addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[v]-60-|" options:0 metrics:nil views:@{@"v" : [self tableView]}]];
+    [[self view] addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[v]|" options:0 metrics:nil views:@{@"v" : [self tableView]}]];
+}
+
+- (void)done:(id)sender
+{
+    [[self delegate] markupControllerDidFinish:self];
+}
+
+- (void)updateView
+{
+    if(([[self userTags] count] > 0 && [self markupType] == STKMarkupTypeUser)
+    || ([[self hashTags] count] > 0 && [self markupType] == STKMarkupTypeHashtag)) {
+        [self setResultsAvailable:YES];
+        [[self tableView] reloadData];
+    } else {
+        [self setResultsAvailable:NO];
+    }
+}
+
+- (void)setResultsAvailable:(BOOL)resultsAvailable
+{
+    if(resultsAvailable) {
+        int count = 0;
+        if([self markupType] == STKMarkupTypeHashtag)
+            count = [[self hashTags] count];
+        else
+            count = [[self userTags] count];
+        
+        if(count > 3)
+            count = 3;
+        [[self view] setFrame:CGRectMake(0, 0, 320, [[self tableView] rowHeight] * count)];
+        [[self tableView] setHidden:NO];
+        [[self promptLabel] setHidden:YES];
+    } else {
+        [[self view] setFrame:CGRectMake(0, 0, 320, 44)];
+        [[self tableView] setHidden:YES];
+        [[self promptLabel] setHidden:NO];
+    }
+}
+
+- (void)textView:(UITextView *)textView updatedWithText:(NSString *)text
+{
+    [self setMarkupRange:NSMakeRange(NSNotFound, 0)];
+    
+    NSRange cursorRange = [textView selectedRange];
+    if(cursorRange.length == 0) {
+        // Then we are in 'cursor mode'
+        NSRegularExpression *exp = [[NSRegularExpression alloc] initWithPattern:@"([#@])([A-Za-z0-9]*)"
+                                                                        options:0
+                                                                          error:nil];
+        [exp enumerateMatchesInString:textView.text options:0 range:NSMakeRange(0, textView.text.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+            NSRange r = [result range];
+            if(r.location != NSNotFound) {
+                if(cursorRange.location > r.location && cursorRange.location <= r.location + r.length) {
+                    NSString *markupCharacter = [[textView text] substringWithRange:[result rangeAtIndex:1]];
+                    if([markupCharacter isEqualToString:@"@"]) {
+                        [self setMarkupType:STKMarkupTypeUser];
+                    } else {
+                        [self setMarkupType:STKMarkupTypeHashtag];
+                    }
+                    [self setMarkupRange:[result rangeAtIndex:2]];
+                    *stop = YES;
+                }
+            }
+        }];
+    }
+    
+    if ([self markupRange].location != NSNotFound) {
+        NSString *textBasis = [[textView text] substringWithRange:[self markupRange]];
+        if([self markupType] == STKMarkupTypeHashtag) {
+            [[STKContentStore store] fetchRecommendedHashtags:textBasis completion:^(NSArray *hashtags) {
+                [self setHashTags:hashtags];
+                [self updateView];
+            }];
+        } else {
+            if([textBasis length] > 1) {
+                [[STKUserStore store] searchUserTrustsWithName:textBasis
+                                                    completion:^(NSArray *users, NSError *error) {
+                                                        [self setUserTags:users];
+                                                        [self updateView];
+                                                    }];
+            } else {
+                [self setUserTags:nil];
+                [self updateView];
+            }
+        }
+    } else {
+        [self setHashTags:nil];
+        [self setUserTags:nil];
+        [self updateView];
+    }
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [cell setBackgroundColor:[UIColor clearColor]];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if([self markupType] == STKMarkupTypeHashtag) {
+        return [[self hashTags] count];
+    }
+    
+    return [[self userTags] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if([self markupType] == STKMarkupTypeUser) {
+        
+        STKSearchProfileCell *cell = [STKSearchProfileCell cellForTableView:tableView target:[self tableView]];
+        [[cell nameLabel] setTextColor:STKTextColor];
+        [[cell nameLabel] setFont:STKFont(14)];
+        [[cell followButton] setHidden:YES];
+        STKUser *user = [[self userTags] objectAtIndex:indexPath.row];
+        [[cell avatarView] setUrlString:[user profilePhotoPath]];
+        [[cell nameLabel] setText:[user name]];
+
+        return cell;
+    } else if([self markupType] == STKMarkupTypeHashtag) {
+        
+        STKSearchHashTagsCell *c = [STKSearchHashTagsCell cellForTableView:tableView target:self];
+        NSString *hashTag = [[self hashTags] objectAtIndex:[indexPath row]];
+        [[c hashTagLabel] setText:[NSString stringWithFormat:@"#%@", hashTag]];
+        [[c count] setText:@""];
+
+        return c;
+    }
+    return nil;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if([self markupType] == STKMarkupTypeHashtag) {
+        NSString *hashTag = [[self hashTags] objectAtIndex:[indexPath row]];
+        [self didSelectHashTag:hashTag];
+        [self setHashTags:nil];
+    } else {
+        STKUser *user = [[self userTags] objectAtIndex:[indexPath row]];
+        [self didSelectUserTag:user];
+        [self setUserTags:nil];
+    }
+    [self updateView];
+}
+
+- (void)didSelectHashTag:(NSString *)hashTag
+{
+    [[self delegate] markupController:self didSelectHashTag:hashTag forMarkerAtRange:[self markupRange]];
+}
+
+- (void)didSelectUserTag:(STKUser *)user
+{
+    [[self delegate] markupController:self didSelectUser:user forMarkerAtRange:[self markupRange]];
+    //if([self userTagRange].location != NSNotFound){
+//        [[self userTags] addObject:user];
+        /*
+        NSString *nameFormatted = [[[user name] lowercaseString] stringByReplacingOccurrencesOfString:@" " withString:@"_"];
+        NSString *replaceWithName = [NSString stringWithFormat:@"%@", nameFormatted];
+        [[[self postTextView] textStorage] replaceCharactersInRange:[self userTagRange] withString:replaceWithName];
+        NSRange replNameRange = NSMakeRange([self userTagRange].location + [replaceWithName length], 0);
+        [[self postTextView] setSelectedRange:replNameRange];
+        
+        [self setUserTagRange:NSMakeRange(NSNotFound, 0)];
+        [[self tableView] reloadData];*/
+//    }
+}
+
+- (void)formatPostViewTextTags
+{
+    if([[self userTags] count] != 0){/*
+        NSString *postText = [[self postTextView] text];
+        __block NSString *updatePostText = [postText copy];
+        __block NSInteger index = 0;
+        NSRegularExpression *tagRegex = [[NSRegularExpression alloc] initWithPattern:@"@([A-Za-z0-9_]*)" options:0 error:nil];
+        [tagRegex enumerateMatchesInString:postText
+                                   options:0
+                                     range:NSMakeRange(0, [postText length])
+                                usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+                                    
+                                    if([[self userTags] objectAtIndex:index]){
+                                        NSRange range = [result rangeAtIndex:1];
+                                        NSString *textToReplace = [postText substringWithRange:range];
+                                        NSString *newValue = [[[self userTags] objectAtIndex:index] uniqueID];
+                                        updatePostText = [updatePostText stringByReplacingOccurrencesOfString:textToReplace withString:newValue];
+                                        index++;
+                                    }
+                                    
+                                }];
+        
+        if(![updatePostText isEqualToString:postText]){
+            [self setTempPostTextWithUserTags:[[self postTextView] text]];
+            [[self postTextView] setText:updatePostText];
+        }*/
+    }
+}
+
+
+@end
