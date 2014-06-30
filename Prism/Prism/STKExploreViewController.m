c67166f55bf93ddf217c097e018a63596c08e3d1
DJ Hayden <dj.hayden@stablekernel.com>
2014-06-30 12:06:58 -0700

    switched fitler/search button positions in nav bar

--- Prism/Prism/STKExploreViewController.m
+++ Prism/Prism/STKExploreViewController.m
@@ -343,7 +343,7 @@
     } else {
         UIBarButtonItem *bbiSearch = [[UIBarButtonItem alloc] initWithCustomView:[self searchButton]];
         UIBarButtonItem *bbiFilter = [[UIBarButtonItem alloc] initWithCustomView:[self filterButton]];
-        [[self navigationItem] setRightBarButtonItems:@[bbiFilter, bbiSearch]];
+        [[self navigationItem] setRightBarButtonItems:@[bbiSearch, bbiFilter]];
         if([self isFilterActive]) {
             [[self filterButton] setSelected:YES];
         }
