e90c29e8f232ee5d209395e4f5a35db5d58f001e
Jonathan Boone <jb@moonshineinteractive.com>
2014-07-02 11:28:39 -0500

    testing and fix of bad lat-lon post for upload data, built 64bit version of Amazon SDKs

--- Prism/Prism/UIImage+STKLocation.m
+++ Prism/Prism/UIImage+STKLocation.m
@@ -16,32 +16,39 @@
 + (void)LocationCoordinateFromImageInfo:(NSDictionary *)imageInfo completion:(void(^)(NSError *, CLLocationCoordinate2D))completion
 {
     ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-    __block CLLocationCoordinate2D locationCoordinate;
+    __block CLLocationCoordinate2D locationCoordinate = locationCoordinate = CLLocationCoordinate2DMake(0, 0);
     [library assetForURL:[imageInfo objectForKey:UIImagePickerControllerReferenceURL] resultBlock:^(ALAsset *asset) {
         
         ALAssetRepresentation *rep = [asset defaultRepresentation];
         uint8_t *buffer = (Byte *)malloc((uint)rep.size);
         NSError *lenErr = nil;
         NSUInteger length = [rep getBytes:buffer fromOffset:0.0 length:(uint)rep.size error:&lenErr];
+        NSError *err = nil;
         if (length != 0){
             NSData *data = [[NSData alloc] initWithBytesNoCopy:buffer length:(uint)rep.size freeWhenDone:YES];
             NSDictionary *sourceOptionsDict = [NSDictionary dictionaryWithObjectsAndKeys:(id)[rep UTI], kCGImageSourceTypeIdentifierHint, nil];
             CGImageSourceRef source = CGImageSourceCreateWithData((__bridge CFDataRef)data, (__bridge CFDictionaryRef)sourceOptionsDict);
             CFDictionaryRef imageProperties = CGImageSourceCopyPropertiesAtIndex(source, 0, NULL);
             CFDictionaryRef gps = (CFDictionaryRef)CFDictionaryGetValue(imageProperties, kCGImagePropertyGPSDictionary);
-            NSNumber *latitude = CFDictionaryGetValue(gps, kCGImagePropertyGPSLatitude);
-            NSNumber *longitude = CFDictionaryGetValue(gps, kCGImagePropertyGPSLongitude);
-            NSString *latRef = CFDictionaryGetValue(gps, kCGImagePropertyGPSLatitudeRef);
-            NSString *lonRef = CFDictionaryGetValue(gps, kCGImagePropertyGPSLongitudeRef);
-            if ([latRef isEqualToString:@"S"]) {
-                latitude = [NSNumber numberWithDouble:-[latitude doubleValue]];
+            if (gps) {
+                NSNumber *latitude = CFDictionaryGetValue(gps, kCGImagePropertyGPSLatitude);
+                NSNumber *longitude = CFDictionaryGetValue(gps, kCGImagePropertyGPSLongitude);
+                NSString *latRef = CFDictionaryGetValue(gps, kCGImagePropertyGPSLatitudeRef);
+                NSString *lonRef = CFDictionaryGetValue(gps, kCGImagePropertyGPSLongitudeRef);
+                if ([latRef isEqualToString:@"S"]) {
+                    latitude = [NSNumber numberWithDouble:-[latitude doubleValue]];
+                }
+                if ([lonRef isEqualToString:@"W"]) {
+                    longitude = [NSNumber numberWithDouble:-[longitude doubleValue]];
+                }
+                locationCoordinate = CLLocationCoordinate2DMake(latitude.doubleValue, longitude.doubleValue);
+            } else {
+                err = [NSError errorWithDomain:@"STKLocation" code:5001 userInfo:nil];
             }
-            if ([lonRef isEqualToString:@"W"]) {
-                longitude = [NSNumber numberWithDouble:-[longitude doubleValue]];
-            }
-            locationCoordinate = CLLocationCoordinate2DMake(latitude.doubleValue, longitude.doubleValue);
+        } else {
+            err = [NSError errorWithDomain:@"STKLocation" code:5002 userInfo:nil];
         }
-        completion(nil, locationCoordinate);
+        completion(err, locationCoordinate);
         
     } failureBlock:^(NSError *error) {
         completion(error, locationCoordinate);
